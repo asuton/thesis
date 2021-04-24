993192fea793d39880d46275ca63b6dba6e52261
Anita Suton <suton.anita@gmail.com>
2021-04-24 14:54:32 +0200

    Added dependencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
+import cookieSession from "cookie-session";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import "reflect-metadata";
 import {
@@ -17,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(morgan("tiny"));
+app.use(cookieSession({ name: "session", maxAge: 24 * 60 * 60 * 1000 }));
+app.use(cookieParser());
 
 app.get("/", (_req: Request, res: Response) => res.send("API Running"));
 app.use("/patients", patientRoute);
